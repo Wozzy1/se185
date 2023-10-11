3d1f89a65200e383158ce3e3bf6965b1a0c5e714
ervin54 <147662765+ervin54@users.noreply.github.com>
2023-10-11 16:09:55 -0500

    Merged changes from questions&solutions into midterm_project

--- midtermProject.c
+++ midtermProject.c
@@ -15,6 +15,7 @@
 -----------------------------------------------------------------------------*/
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /*----------------------------------------------------------------------------
 -	                            Functions Prototypes                                   -
@@ -61,7 +62,7 @@ int quiz2()
 	int score = 0;
 	char answer1[10];
 	printf("\nThe following code produces a compilation error: int m_idterm_project2023 = 3; (true/false)");
-	scanf("%s, answer2); //true
+	scanf("%s", answer1); //true
 
 	char answer2[10];
 	printf("\nWhich of the following are not string formats?\n(a) %s\n(b) %c\n(c) %If\n(d) %s");
@@ -69,7 +70,7 @@ int quiz2()
 	
 	char answer3[50];
 	printf("\nWithin C coding 0 respresents false and __ respresents true");
-	scanf("%s", answer2); //1
+	scanf("%s", answer3); //1
 	
 }
 
@@ -78,7 +79,7 @@ int quiz3()
 	int score = 0;
 	char answer1[10];
 	printf("\nA for loop and a while loop are written the same way, but they have to use different variables (true/false)");
-	scanf("%s, answer2); //false
+	scanf("%s", answer1); //false
 
 	char answer2[10];
 	printf("\nWhich number is divisible by 3? \n(a) 299\n(b) 341\n(c) 103\n(d) 1488");
@@ -86,7 +87,7 @@ int quiz3()
 	
 	char answer3[50];
 	printf("\nDuring a _______ statement the code takes the value given by the user and assigns it to a variable");
-	scanf("%s", answer2); //scanf
+	scanf("%s", answer3); //scanf
 	
 }
 
