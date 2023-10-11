6c47abd198dfd9ae8920e17ce83e4ddba3b2a15f
Wozzy1 <134657893+Wozzy1@users.noreply.github.com>
2023-10-11 17:41:12 -0500

    add more spaces nathan

--- midtermProject.c
+++ midtermProject.c
@@ -41,58 +41,62 @@ int main()
 {
 	//SOME LOOP HERE TO KEEP THE PROGRAM GOING UNTIL PERFECT SCORE
 	printf("\nThis is Midterm Exam 1");
-	
-	printf("\nEnter a random number (1-9)");
+	int num;
+	printf("\nEnter a random number (1-9) ");
 	scanf("%d", &num);
-	srand(time(num));
+	//srand(time(num));
 	
-	bool complete = false;
+	bool allComplete = false;
+	bool complete1 = false;
+	bool complete2 = false;
+	bool complete3 = false;
 	bool isFinished = false; // true means done | false means continue
 	int randNum = rand() % 3 + 1;
 	
 	printf("\nYou were randomly selected to start with Exam %d", randNum);
 	printf("\n\n\n\n\n\n");
 	
-	while (!complete && !isFinished)
+	while (!allComplete || !isFinished)
 	{
 		
 		if (randNum == 1)
 		{
-			quiz1();
+			score = quiz1();
 			
 			if (score == 15)
 			{
-				complete = true;
+				complete1 = true;
 			}
 		}
 		
 		else if (randNum == 2)
 		{
-			quiz2();
+			score = quiz2();
 			
 			if (score == 15)
 			{
-				complete = true;
+				complete2 = true;
 			}
 		}
 		
 		else if (randNum == 3)
 		{
-			quiz3();
+			score = quiz3();
 			
 			if (score == 15)
 			{
-				complete = true;
+				complete3 = true;
 			}
 		}
 		
-		if (complete)
+		if (complete1 || complete2 || complete3)
 		{
-			char response[10];
+			int response;
 			printf("\nNice job. Now you can choose to take another quiz or be done");
-			printf("\nTo take another quiz, enter the number (1, 2, 3) quiz you want to take and to be done, enter '0'");
-			scanf("%d", response);
+			printf("\nTo take another quiz, enter the number (1, 2, 3) quiz you want to take and to be done, enter '0' ");
+			scanf("%d", &response);
 			
+			printf("\n\n%d\n\n", response);
 			if (response == 0)
 			{
 				break;
@@ -114,6 +118,7 @@ int main()
 			}
 		}
 		
+		allComplete = complete1 && complete2 && complete3;
 	}
 	
 	// blah blah blah final score
@@ -129,34 +134,73 @@ int quiz1()
 {
 	int score = 0;
 	char answer1[10];
-	printf("\naX+b is a linear combination (true/false)");
+	printf("\naX+b is a linear combination (true/false) ");
 	scanf("%s", answer1); //true
 
 	char answer2[10];
-	printf("\nWhich is a prime number?\n(a) 39\n(b) 37\n(c) 81\n(d) 99");
-	scanf("%s", answer2); //a
+	printf("\nWhich is a prime number?\n(a) 39\n(b) 37\n(c) 81\n(d) 99 ");
+	scanf("%s", answer2); //b
 	
 	char answer3[50];
-	printf("\nThe ________ sequence is a series of numbers where each number is the sum of the two preceding ones, usually starting with 0 and 1");
+	printf("\nThe ________ sequence is a series of numbers where each number is the sum of the two preceding ones, usually starting with 0 and 1 ");
 	scanf("%s", answer3); //fibonacci
 	
+    if (strcmp(answer1, "true") == 0) {
+        score = 5 + score;
+    } else {
+        printf("You got question 1 incorrect\n");
+    }
+
+    if (answer2[0] == 'b') {
+        score = 5 + score;
+    } else {
+        printf("You got question 2 incorrect\n");
+    }
+
+    if (strcmp(answer3, "fibonacci") == 0) {
+        score = 5 + score;
+    } else {
+        printf("You got question 3 incorrect\n");
+    }
+
+	return score;
+
 }
 
 int quiz2()
 {
 	int score = 0;
 	char answer1[10];
-	printf("\nThe following code produces a compilation error: int m_idterm_project2023 = 3; (true/false)");
-	scanf("%s", answer1); //true
+	printf("\nThe following code produces a compilation error: int m_idterm_project2023 = 3; (true/false) ");
+	scanf("%s", answer1); //false
 
 	char answer2[10];
-	printf("\nWhich of the following are not string formats?\n(a) %s\n(b) %c\n(c) %If\n(d) %s");
+	printf("\nWhich of the following are not string formats?\n(a) %s\n(b) %c\n(c) %If\n(d) %s ");
 	scanf("%s", answer2); //c
 	
 	char answer3[50];
-	printf("\nWithin C coding 0 respresents false and __ respresents true");
+	printf("\nWithin C coding 0 respresents false and __ respresents true ");
 	scanf("%s", answer3); //1
 	
+    if (strcmp(answer1, "false") == 0) {
+        score = 5 + score;
+    } else {
+        printf("You got question 1 incorrect\n");
+    }
+
+    if (answer2[0] == 'c') {
+        score = 5 + score;
+    } else {
+        printf("You got question 2 incorrect\n");
+    }
+
+    if (strcmp(answer3, "1") == 0) {
+        score = 5 + score;
+    } else {
+        printf("You got question 3 incorrect\n");
+    }
+
+	return score;
 }
 
 int quiz3()
@@ -174,6 +218,25 @@ int quiz3()
 	printf("\nDuring a _______ statement the code takes the value given by the user and assigns it to a variable");
 	scanf("%s", answer3); //scanf
 	
+    if (strcmp(answer1, "false") == 0) {
+        score = 5 + score;
+    } else {
+        printf("You got question 1 incorrect\n");
+    }
+
+    if (answer2[0] == 'd') {
+        score = 5 + score;
+    } else {
+        printf("You got question 2 incorrect\n");
+    }
+
+    if (strcmp(answer3, "scanf") == 0) {
+        score = 5 + score;
+    } else {
+        printf("You got question 3 incorrect\n");
+    }
+
+	return score;
 }
 
 void convertToLowercase(char *str) {
